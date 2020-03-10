db8a5f57cdd1ddb2f108356ef5f3e932f5602a9c
RPN <Ricnolan@gmail.com>
2020-03-10 19:27:23 +0000

    Added globals to control FPS dimensions

--- SDLGameEngine/Constants.h
+++ SDLGameEngine/Constants.h
@@ -7,6 +7,9 @@ const bool G_DEBUG = true;
 const unsigned int G_WINDOW_WIDTH = 1600;
 const unsigned int G_WINDOW_HEIGHT = 900;
 
+const unsigned int G_FPS_COUNTER_WIDTH = 100;
+const unsigned int G_FPS_COUNTER_HEIGHT = 21;
+
 const unsigned int G_TARGET_FPS = 120;
 const float G_TARGET_DELTA_MS = 1000.0f / G_TARGET_FPS;
 
